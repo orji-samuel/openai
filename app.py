89094332ea1fe348abd470d2db90c304ff5bcab9
orji-samuel <orjis553@gmail.com>
2026-01-27 20:58:23 +0100

    python update

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Python is working")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Flask is running"
+
+if __name__== "__main__":
+    app.run(debug=True)
